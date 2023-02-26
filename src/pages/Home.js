40a555e7e6760cffc1d1980c62936b1416216ffd
Henry Winters <hlwinters17@gmail.com>
2023-02-26 13:33:32 -0800

    updated readme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,34 +58,3 @@ const Home = ({ user, workouts, setWorkouts, following, setFollowing, setNotific
 }
 
 export default Home 
-
-/*
-const Home = ({ user, workouts, setWorkouts, following, setFollowing }) => {
-
-    useEffect(() => {
-        const getWorkouts = async () => {
-            const userAndFollowingWorkouts = await workoutService.getUserAndFollowingWorkouts(user.username)
-            setWorkouts(userAndFollowingWorkouts)
-        } 
-        getWorkouts()
-    }, [])
-
-    if (workouts.length >= 1) {
-        return (
-            <div className='workouts-container-profile'>
-                {workouts.map(workout => 
-                    <Workout key={workout.id} workout={workout} user={user} setWorkouts={setWorkouts} following={following} setFollowing={setFollowing} /> 
-                )}
-            </div> 
-        )
-    } else {
-        return (
-            <div className='no-workouts-to-display-profile'> 
-                <p>No workouts yet! Click "Add Workout" to start your first log.</p> 
-            </div> 
-        )
-    }
-}
-
-export default Home 
-*/
